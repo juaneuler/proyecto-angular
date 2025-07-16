bcef62f6e8cd9cc351dbeb609cd58bc28b2c41e0
Juan Euler <juaneuler@hotmail.com>
2025-07-16 19:17:25 -0300

    Cambios a app para hacer fetch al JsON y mostrar la tabla de estudiantes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { Home } from './home/home';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../shared/entities';
+import { Navbar } from './navbar/navbar';
+import { Toolbar } from './toolbar/toolbar';
+import { StudentsTable } from "./students-table/students-table";
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
+  imports: [CommonModule, Navbar, Toolbar, StudentsTable],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected title = 'angular-EULER-Juan';
+export class App implements OnInit {
+  students: Student[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Student[]>('/mocks/students.json').subscribe((data) => {
+      this.students = data;
+    });
+  }
 }
