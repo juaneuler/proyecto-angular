5c4439bebdc593a50d90c2f8067e5c67360c0f10
Juan Euler <juaneuler@hotmail.com>
2025-07-16 16:42:40 -0300

    Cambios a Home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
-import { Student } from '../student/student';
 import { Toolbar } from '../toolbar/toolbar';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, Navbar, Student, Toolbar],
+  imports: [CommonModule, Navbar, Toolbar],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
