f04b41b5f5314f6a44c8244fcc728ca33bf84499
Juan Euler <juaneuler@hotmail.com>
2025-07-17 15:31:41 -0300

    Componente edit-form

---
+++ src/app/edit-form/edit-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-form',
+  imports: [],
+  templateUrl: './edit-form.html',
+  styleUrl: './edit-form.scss'
+})
+export class EditForm {
+
+}
