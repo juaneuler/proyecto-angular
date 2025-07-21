f2bb208fa5d7f26dfaad3ae2bded40405a1fd12b
Juan Euler <juaneuler@hotmail.com>
2025-07-21 15:27:58 -0300

    refactor(delete-form): se eliminó la lógica de notificación con snackbar en el formulario

--- src/app/delete-form/delete-form.ts
+++ src/app/delete-form/delete-form.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -14,7 +13,6 @@ import { Bigtitle } from '../../shared/directives/bigtitle';
   styleUrl: './delete-form.scss',
 })
 export class DeleteForm implements OnInit {
-  private _snackBar = inject(MatSnackBar);
 
   @Output() studentDeleted = new EventEmitter<string>();
 
@@ -31,7 +29,6 @@ export class DeleteForm implements OnInit {
 
   onDelete() {
     if (this.studentForm.valid) {
-      this.showDeletedSuccesfully();
       const { dni } = this.studentForm.value;
       this.studentDeleted.emit(dni);
       this.onReset();
@@ -40,17 +37,5 @@ export class DeleteForm implements OnInit {
 
   onReset() {
     this.studentForm.reset();
-  }
-
-  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
-  verticalPosition: MatSnackBarVerticalPosition = 'top';
-
-  showDeletedSuccesfully() {
-    const message = 'Estudiante eliminado exitosamente!';
-    const action = 'Cerrar';
-    this._snackBar.open(message, action, {
-      horizontalPosition: this.horizontalPosition,
-      verticalPosition: this.verticalPosition,
-    });
   }
 }
