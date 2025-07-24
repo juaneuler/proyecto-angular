9e602185635f72fae027d32cc706115ab2eabadb
Juan Euler <juaneuler@hotmail.com>
2025-07-24 16:34:25 -0300

    refactor(app): eliminar lógica de notificaciones duplicada
    - Se eliminó el llamado a snackbar local y se delegó al nuevo servicio centralizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,23 +4,34 @@ import { HttpClient } from '@angular/common/http';
 import { Student } from '../shared/entities';
 import { Navbar } from './navbar/navbar';
 import { Toolbar } from './toolbar/toolbar';
-import { StudentsTable } from "./students-table/students-table";
-import { AddForm } from "./add-form/add-form";
+import { StudentsTable } from './students-table/students-table';
+import { AddForm } from './add-form/add-form';
 import { DeleteForm } from './delete-form/delete-form';
-import { EditForm } from "./edit-form/edit-form";
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { EditForm } from './edit-form/edit-form';
+import { SnackbarNotification } from '../shared/services/snackbar-notification';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, Navbar, Toolbar, StudentsTable, AddForm, DeleteForm, EditForm, MatSnackBarModule],
+  imports: [
+    CommonModule,
+    Navbar,
+    Toolbar,
+    StudentsTable,
+    AddForm,
+    DeleteForm,
+    EditForm,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
   students: Student[] = [];
-  activeSection = "students"
+  activeSection = 'students';
 
-  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
+  constructor(
+    private http: HttpClient,
+    private snackbarNotification: SnackbarNotification
+  ) {}
 
   ngOnInit(): void {
     this.http.get<Student[]>('/mocks/students.json').subscribe((data) => {
@@ -29,34 +40,33 @@ export class App implements OnInit {
   }
 
   addStudent(student: Student) {
-    this.students = [...this.students, {...student, dni: Number(student.dni)}]
+    this.students = [
+      ...this.students,
+      { ...student, dni: Number(student.dni) },
+    ];
   }
 
   deleteStudent(dni: string) {
-  const index = this.students.findIndex(student => student.dni === Number(dni));
+    const index = this.students.findIndex(
+      (student) => student.dni === Number(dni)
+    );
 
-  if (index !== -1) {
-    this.students.splice(index, 1);
-    this.students = [...this.students];
-    this.snackBar.open('Estudiante eliminado exitosamente!', 'Cerrar', {
-      duration: 3000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top'
-    });
-  } else {
-    this.snackBar.open('No se encontró ningún estudiante con ese DNI.', 'Cerrar', {
-      duration: 3000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top'
-    });
+    if (index !== -1) {
+      this.students.splice(index, 1);
+      this.students = [...this.students];
+      this.snackbarNotification.success('Estudiante eliminado exitosamente!');
+    } else {
+      this.snackbarNotification.error(
+        'No se encontró ningún estudiante con ese DNI.'
+      );
+    }
   }
-}
 
   editStudent(editedStudent: Student) {
-  this.students = this.students.map(student =>
-    student.dni === Number(editedStudent.dni)
-      ? { ...editedStudent, dni: Number(editedStudent.dni) }
-      : student
-  );
-}
+    this.students = this.students.map((student) =>
+      student.dni === Number(editedStudent.dni)
+        ? { ...editedStudent, dni: Number(editedStudent.dni) }
+        : student
+    );
+  }
 }
