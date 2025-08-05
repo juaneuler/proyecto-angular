c289135887ab4f4022ce5ce9dc0c372b452774a0
Juan Euler <juaneuler@hotmail.com>
2025-08-05 00:40:35 -0300

    feat(inscripciones): crear componente contenedor para manejar tabla y formularios de inscripciones

---
+++ src/app/features/inscripciones/containers/inscripciones/inscripciones.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Observable, combineLatest, map } from 'rxjs';
+
+import { AppRoutes } from '../../../../../shared/enums/routes';
+import { InscriptionsTable } from '../../components/inscriptions-table/inscriptions-table';
+import { InscripcionDetalle } from '../../../../../shared/entities';
+import { InscripcionesEstadoService } from '../../inscripciones-estado';
+import { AlumnosState } from '../../../alumnos/alumnos-estado';
+import { CursosState } from '../../../cursos/cursos-estado';
+
+@Component({
+  selector: 'app-inscripciones',
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet,
+    InscriptionsTable,
+  ],
+  templateUrl: './inscripciones.html',
+  styleUrl: './inscripciones.scss',
+})
+export class Inscripciones implements OnInit {
+  AppRoutes = AppRoutes;
+
+  inscripcionesDetalladas$!: Observable<InscripcionDetalle[]>;
+
+  constructor(
+    private inscripcionesEstado: InscripcionesEstadoService,
+    private alumnosState: AlumnosState,
+    private cursosState: CursosState
+  ) {}
+
+  ngOnInit(): void {
+    this.inscripcionesDetalladas$ = combineLatest([
+      this.inscripcionesEstado.inscripciones$,
+      this.alumnosState.students$,
+      this.cursosState.cursos$,
+    ]).pipe(
+      map(([inscripciones, alumnos, cursos]) => {
+        return inscripciones.map((insc) => {
+          const alumno = alumnos.find((a) => a.dni.toString() === insc.alumnoDNI);
+          const curso = cursos.find((c) => c.code === insc.cursoCodigo);
+
+          return {
+            alumnoDNI: alumno?.dni ?? 0,
+            alumnoNombre: alumno?.name ?? 'N/D',
+            alumnoApellido: alumno?.surname ?? 'N/D',
+            cursoCodigo: curso?.code ?? 'N/D',
+            cursoNombre: curso?.name ?? 'N/D',
+          };
+        });
+      })
+    );
+  }
+}
