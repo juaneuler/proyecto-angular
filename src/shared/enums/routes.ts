3d9ca03f1ffd9e48a9755a1574136bfd91dde500
Juan Euler <juaneuler@hotmail.com>
2025-08-05 00:24:46 -0300

    feat(routing): crear archivo de rutas y configurar navegación a través de enums

---
+++ src/shared/enums/routes.ts
@@ -0,0 +1,20 @@
+export enum AppRoutes {
+  Home = '',
+  Alumnos = 'alumnos',
+  AlumnosAgregar = 'agregar',
+  AlumnosEditar = 'editar',
+  AlumnosEliminar = 'eliminar',
+  Cursos = 'cursos',
+  CursosAgregar = 'alta',
+  CursosEditar = 'edicion',
+  CursosEliminar = 'baja',
+  Inscripciones = 'inscripciones',
+  InscripcionesAgregar = 'agregar',
+  InscripcionesEditar = 'modificar',
+  InscripcionesEliminar = 'borrar',
+}
+
+export enum ApiRoutes {
+  Students = 'students',
+  Courses = 'courses',
+}
