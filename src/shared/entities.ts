a4c9d0b09eba44e15499ca01727413889309ee1b
Juan Euler <juaneuler@hotmail.com>
2025-08-05 00:41:54 -0300

    feat(entities): agregar nuevas interfaces para cursos, alumnos e inscripciones

--- src/shared/entities.ts
+++ src/shared/entities.ts
@@ -4,4 +4,36 @@ export interface Student {
     age: number;
     dni: number;
     average: number;
+    id: string;
+}
+
+export interface Course {
+    name: string;
+    code: string;
+    credits: number;
+    id: string;
+}
+
+export interface Inscription {
+    alumnoDNI: string;
+    cursoCodigo: string;
+}
+
+export interface CambioInscripcion {
+  anterior: {
+    alumnoDNI: string;
+    cursoCodigo: string;
+  };
+  nueva: {
+    alumnoDNI: string;
+    cursoCodigo: string;
+  };
+}
+
+export interface InscripcionDetalle {
+  alumnoDNI: number;
+  alumnoNombre: string;
+  alumnoApellido: string;
+  cursoCodigo: string;
+  cursoNombre: string;
 }
