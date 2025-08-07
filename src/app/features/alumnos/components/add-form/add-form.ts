9186aa8a7eb78346b52e5d870fa687f04eeb71a4
Juan Euler <juaneuler@hotmail.com>
2025-08-06 21:24:34 -0300

    feat(add-form): mejora la validación de los campos del formulario
    Añade validaciones más específicas para los campos 'name' y 'surname' (minLength, maxLength y pattern).
    También ajusta las validaciones de 'age' y 'average' para asegurar datos más consistentes.
    Esto mejora la experiencia del usuario y la calidad de los datos del formulario.

--- src/app/features/alumnos/components/add-form/add-form.ts
+++ src/app/features/alumnos/components/add-form/add-form.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Bigtitle } from '../../../../../shared/directives/bigtitle';
 import { SnackbarNotification } from '../../../../../shared/services/snackbar-notification';
 import { AlumnosState } from '../../alumnos-estado';
@@ -14,27 +19,55 @@ import { AppRoutes } from '../../../../../shared/enums/routes';
   styleUrl: './add-form.scss',
 })
 export class AddForm implements OnInit {
-  readonly routes = AppRoutes; 
+  readonly routes = AppRoutes;
 
   private snackbarNotification = inject(SnackbarNotification);
 
   studentForm!: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private alumnosState: AlumnosState
-  ) {}
+  constructor(private fb: FormBuilder, private alumnosState: AlumnosState) {}
 
   ngOnInit() {
     this.studentForm = this.fb.group({
-      dni: ['', [
-    Validators.required,
-    Validators.pattern(/^[1-9]\d{6,7}$/) // Esto lo agrego para que en DNI ingresado tenga si o si entre 7 y 8 dígitos, y que no empiece con 0
-    ]],
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      age: ['', [Validators.required, Validators.min(1), Validators.pattern(/^\d+$/)]],
-      average: ['', [Validators.required, Validators.min(0.1), Validators.max(10)]],
+      dni: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[1-9]\d{6,7}$/), // DNI con 7 u 8 dígitos, y no puede empezar con 0
+        ],
+      ],
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2), // Longitud mínima de 2 caracteres
+          Validators.maxLength(50), // Longitud máxima de 50 caraceteres
+          Validators.pattern(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/), // Solo letras, espacios y tildes
+        ],
+      ],
+      surname: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),  // Longitud mínima de 2 caracteres
+          Validators.maxLength(50), // Longitud máxima de 50 caracteres
+          Validators.pattern(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/),  // Solo letras, espacios y tildes
+        ],
+      ],
+      age: [
+        '',
+        [
+        Validators.required, 
+        Validators.min(1),  // Que la edad sea al menos 1 año
+        Validators.pattern(/^\d+$/)],
+      ],
+      average: [
+        '',
+        [
+          Validators.required, 
+          Validators.min(1),  // Que el promedio sea siempre superior a 1
+          Validators.max(10)],  // Que el promedio sea como máximo 10
+      ],
     });
   }
 
