0f2407051c5568441869e6e19bc609ff7d18104c
Juan Euler <juaneuler@hotmail.com>
2025-08-10 17:09:34 -0300

    refactor(app.routes.ts): Se unificó la configuración de rutas, lazy loading y guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AppRoutes } from '../shared/enums/routes';
 import { Home } from './home/home';
 import { NotFound } from './not-found/not-found';
+import { adminGuard } from '../shared/guards/admin-guard';
+import { NotAuthorized } from './not-authorized/not-authorized';
 
 export const routes: Routes = [
   {
@@ -10,15 +12,24 @@ export const routes: Routes = [
   },
   {
     path: AppRoutes.Alumnos,
-    loadChildren: () => import('./features/alumnos/alumnos.routes').then(m => m.ALUMNOS_ROUTES),
+    canActivate: [adminGuard], // Esto protege la ruta con el guard!
+    loadChildren: () =>
+      import('./features/alumnos/alumnos.routes').then((m) => m.ALUMNOS_ROUTES),
   },
   {
     path: AppRoutes.Cursos,
-    loadChildren: () => import('./features/cursos/cursos.routes').then(m => m.CURSOS_ROUTES),
+    canActivate: [adminGuard], // Esto protege la ruta con el guard!
+    loadChildren: () =>
+      import('./features/cursos/cursos.routes').then((m) => m.CURSOS_ROUTES),
   },
   {
     path: AppRoutes.Inscripciones,
-    loadChildren: () => import('./features/inscripciones/inscripciones.routes').then(m => m.INSCRIPCIONES_ROUTES),
+    canActivate: [adminGuard], // Esto protege la ruta con el guard!
+    loadChildren: () =>
+      import('./features/inscripciones/inscripciones.routes').then(
+        (m) => m.INSCRIPCIONES_ROUTES
+      ),
   },
+  { path: AppRoutes.Unauthorized, component: NotAuthorized },
   { path: '**', component: NotFound },
 ];
