07b386d18d40f2953a9170f2481e966571b32e2a
Juan Euler <juaneuler@hotmail.com>
2025-08-13 21:17:24 -0300

    test: se ajustó fullname-pipe.spec.ts para que pase ng test
    Se corrigió la lógica de validación en la prueba unitaria de FullnamePipe,
    asegurando que los valores de entrada y salida coincidan con la funcionalidad real.

--- src/shared/pipes/fullname-pipe.spec.ts
+++ src/shared/pipes/fullname-pipe.spec.ts
@@ -1,8 +1,24 @@
-import { FullnamePipe } from './fullname-pipe';
+import { FullnamePipe } from "./fullname-pipe";
 
 describe('FullnamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FullnamePipe();
+
+  let pipe: FullnamePipe;
+
+  beforeEach(() => {
+    pipe = new FullnamePipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should transform a name and surname into a full name', () => {
+    const name = 'Juan';
+    const surname = 'Perez';
+    const expectedResult = 'Juan Perez';
+
+    const result = pipe.transform(name, surname);
+
+    expect(result).toBe(expectedResult);
   });
 });
