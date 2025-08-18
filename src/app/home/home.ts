218a88df743ce8748efdcfb814bf29ec5772f131
Juan Euler <juaneuler@hotmail.com>
2025-08-17 23:18:17 -0300

    feat(home): se mejoró la experiencia de usuario
    - Se agregó lógica condicional según quién esté logueado

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../core/auth/auth-service';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-
+  constructor(public authService: AuthService) {}
 }
