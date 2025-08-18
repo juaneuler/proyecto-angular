10a2bd69ead5aef7a2d5b70eaf27053f594477f8
Juan Euler <juaneuler@hotmail.com>
2025-08-17 23:21:14 -0300

    refactor(guards): se mejoró el tipado en el guard
    - Se agregaron parámetros con tipos explícitos

--- src/shared/guards/admin-guard.ts
+++ src/shared/guards/admin-guard.ts
@@ -1,18 +1,30 @@
-import { CanActivateFn, Router } from '@angular/router';
+import {
+  CanActivateFn,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { inject } from '@angular/core';
+import { AuthService } from '../../app/core/auth/auth-service';
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  // Esto es un guard simple para demostrar que sé aplicar su uso. No hay validación de admin como tal porque mi app no implementa login, pero establecer una variable que tenga el isAdmin en true es suficiente para el ejemplo
-
+export const adminGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService = inject(AuthService);
   const router = inject(Router);
 
-  // Esta es la variable para emular un login de admin
-  const isAdmin = true; // Si cambiamos este booleano a false, se puede probar la funcionalidad!
+  // Si el usuario logueado es admin, entonces permitimos el acceso
+  let isAdmin = false;
+  authService.user$
+    .subscribe((user) => {
+      isAdmin = !!user && user.role == 'admin';
+    })
+    .unsubscribe();
 
   if (isAdmin) {
-    return true; // Como isAdmin es true, se puede acceder
+    return true;
   } else {
-    // El usuario no es admin, por lo que es redigirido a un componente de error!
     return router.createUrlTree(['/unauthorized']);
   }
 };
