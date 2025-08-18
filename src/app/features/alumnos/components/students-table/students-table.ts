d5e386c12967549ca29192d7016c4b728890fb00
Juan Euler <juaneuler@hotmail.com>
2025-08-17 22:43:40 -0300

    refactor(alumnos): optimizar y mejorar tipado
    - Se agregó el manejo de "sin datos"

--- src/app/features/alumnos/components/students-table/students-table.ts
+++ src/app/features/alumnos/components/students-table/students-table.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { FullnamePipe } from './../../../../../shared/pipes/fullname-pipe';
 import { Student } from './../../../../../shared/entities';
+import { CommonModule } from '@angular/common';
+import { Bigtitle } from '../../../../../shared/directives/bigtitle';
 
 
 @Component({
   selector: 'app-students-table',
-  imports: [MatTableModule, FullnamePipe],
+  imports: [MatTableModule, FullnamePipe, CommonModule, Bigtitle],
   templateUrl: './students-table.html',
   styleUrl: './students-table.scss'
 })
