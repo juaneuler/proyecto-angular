9ada8e3b1099d6ea1a1788ef4ba30631d1329165
Juan Euler <juaneuler@hotmail.com>
2025-08-17 23:16:47 -0300

    feat(navbar): se actualizó la navegación
    - Se agregaron rutas de login y gestión de usuarios
    - La gestión de usuarios es solo visible para el administrador
    - Se agregaron botones de login y logout con lógica específica

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Bigtitle } from '../../shared/directives/bigtitle';
 import { AppRoutes } from '../../shared/enums/routes';
+import { AuthService } from '../core/auth/auth-service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [Bigtitle, RouterModule],
+  imports: [Bigtitle, RouterModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
   readonly AppRoutes = AppRoutes;
+
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
