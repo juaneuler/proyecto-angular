34e6f9ef2a6b70292072b22d10630fb7e85f2627
Juan Euler <juaneuler@hotmail.com>
2025-08-17 21:18:16 -0300

    feat(cursos): se mejoró la tabla de cursos con manejo de datos vacíos

--- src/app/features/cursos/components/courses-table/courses-table.ts
+++ src/app/features/cursos/components/courses-table/courses-table.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { Course } from '../../../../../shared/entities';
+import { CommonModule } from '@angular/common';
+import { Bigtitle } from '../../../../../shared/directives/bigtitle';
 
 @Component({
   selector: 'app-courses-table',
-  imports: [MatTableModule],
+  imports: [MatTableModule, CommonModule, Bigtitle],
   templateUrl: './courses-table.html',
   styleUrl: './courses-table.scss'
 })
