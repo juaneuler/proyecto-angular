22c6bf5c0c744c638947b4f79b32eb3b324754d7
Juan Euler <juaneuler@hotmail.com>
2025-08-17 23:11:18 -0300

    feat(routes): se implementó la protección con guards
    - Se agregó el adminGuard a las rutas correspondientes

--- src/app/features/inscripciones/inscripciones.routes.ts
+++ src/app/features/inscripciones/inscripciones.routes.ts
@@ -4,10 +4,11 @@ import { AddInscripcion } from './components/add-inscripcion/add-inscripcion';
 import { EditInscripcion } from './components/edit-inscripcion/edit-inscripcion';
 import { DeleteInscripcion } from './components/delete-inscripcion/delete-inscripcion';
 import { AppRoutes } from '../../../shared/enums/routes';
+import { adminGuard } from '../../../shared/guards/admin-guard';
 
 export const INSCRIPCIONES_ROUTES: Routes = [
   { path: '', component: Inscripciones },
   { path: AppRoutes.InscripcionesAgregar, component: AddInscripcion },
-  { path: AppRoutes.InscripcionesEditar, component: EditInscripcion },
-  { path: AppRoutes.InscripcionesEliminar, component: DeleteInscripcion },
+  { path: AppRoutes.InscripcionesEditar, component: EditInscripcion, canActivate: [adminGuard] },
+  { path: AppRoutes.InscripcionesEliminar, component: DeleteInscripcion, canActivate: [adminGuard] },
 ];
