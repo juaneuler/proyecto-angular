9d715e033d158182bd41617cc0f77337ffe969de
Juan Euler <juaneuler@hotmail.com>
2025-08-17 23:10:46 -0300

    feat(routes): implementar protección con guards
    - Se agregó el adminGuard a las rutas correspondientes

--- src/app/features/cursos/cursos.routes.ts
+++ src/app/features/cursos/cursos.routes.ts
@@ -4,10 +4,11 @@ import { FormAlta } from './components/form-alta/form-alta';
 import { FormEdicion } from './components/form-edicion/form-edicion';
 import { FormBaja } from './components/form-baja/form-baja';
 import { AppRoutes } from '../../../shared/enums/routes';
+import { adminGuard } from '../../../shared/guards/admin-guard';
 
 export const CURSOS_ROUTES: Routes = [
   { path: '', component: Cursos },
   { path: AppRoutes.CursosAgregar, component: FormAlta },
-  { path: AppRoutes.CursosEditar, component: FormEdicion },
-  { path: AppRoutes.CursosEliminar, component: FormBaja },
+  { path: AppRoutes.CursosEditar, component: FormEdicion, canActivate: [adminGuard] },
+  { path: AppRoutes.CursosEliminar, component: FormBaja, canActivate: [adminGuard] },
 ];
