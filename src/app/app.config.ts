baf52770a8e7d0777a5a2208339713a6b6e9243d
Juan Euler <juaneuler@hotmail.com>
2025-08-21 21:44:28 -0300

    feat(config): se agregó configuración de NgRx Store y Store Devtools en app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,16 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { authReducer } from './core/auth/store/auth.reducer';
+import { routesReducer } from './core/auth/routes/store/routes.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +19,13 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideStore({ auth: authReducer, routes: routesReducer }),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: false,
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
   ],
 };
