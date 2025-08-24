de754696a203d7cafa76cbfb840b7616f512e3f9
Juan Euler <juaneuler@hotmail.com>
2025-08-23 22:00:48 -0300

    refactor(toolbar): se usó async pipe para mostrar usuario y título de ruta
    - Se eliminaron las suscripciones manuales en el componente
    - Se utilizó async pipe en el template para currentUser$ y pageTitle$
    - Se mejoró la reactividad y limpieza del código

--- src/app/toolbar/toolbar.ts
+++ src/app/toolbar/toolbar.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { filter, map, switchMap } from 'rxjs/operators';
 import { AuthUser } from '../core/auth/auth.models';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import * as AuthSelectors from '../core/auth/store/auth.selectors';
 import { RoutesService } from '../core/auth/routes/services/routes.service';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
@@ -15,37 +16,35 @@ import { RoutesService } from '../core/auth/routes/services/routes.service';
   styleUrl: './toolbar.scss',
 })
 export class Toolbar implements OnInit {
-  currentUser: AuthUser | null = null;
-  currentRoute: string = '';
-  pageTitle: string = '';
+  currentUser$: Observable<AuthUser | null>;
+  currentRoute$ = new BehaviorSubject<string>('');
+  pageTitle$: Observable<string>;
 
   constructor(
     private router: Router,
     private store: Store,
     private routesService: RoutesService
-  ) {}
+  ) {
+    this.currentUser$ = this.store.select(AuthSelectors.selectUser);
+    this.pageTitle$ = of('Inicio');
+  }
 
   ngOnInit(): void {
-    // Usuario logueado desde NgRx
-    this.store.select(AuthSelectors.selectUser).subscribe((user) => {
-      this.currentUser = user;
-    });
-
     // Ruta actual, usando el servicio de rutas con NgRx
     this.router.events
       .pipe(
         filter(
           (event): event is NavigationEnd => event instanceof NavigationEnd
-        )
+        ),
+        map((event) => event.urlAfterRedirects.replace('/', '') || '')
       )
-      .subscribe((event: NavigationEnd) => {
-        const ruta = event.urlAfterRedirects.replace('/', '');
-        this.currentRoute = ruta || '';
-
-        // Obtener el título del servicio
-        this.routesService
-          .getRouteTitle(this.currentRoute)
-          .subscribe((title) => (this.pageTitle = title));
+      .subscribe((ruta) => {
+        this.currentRoute$.next(ruta);
       });
+
+    // Reasignar pageTitle$ con la lógica completa
+    this.pageTitle$ = this.currentRoute$.pipe(
+      switchMap((route) => this.routesService.getRouteTitle(route))
+    );
   }
 }
