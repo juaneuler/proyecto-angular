cd1d87c275533f0419961e9769c8a71e34b470bb
Juan Euler <juaneuler@hotmail.com>
2025-08-24 15:51:11 -0300

    refactor(alumnos): se migró EditForm a patrón reactivo con async pipe
    - Se reemplazó el acceso sincrónico a datos por observables
    - Se convirtieron propiedades de estado a BehaviorSubject
    - Se implementó async pipe en template para manejo reactivo
    - Se mantuvo el acceso a valores con tap() para búsquedas

--- src/app/features/alumnos/components/edit-form/edit-form.ts
+++ src/app/features/alumnos/components/edit-form/edit-form.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, inject } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
-  FormControl,
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
@@ -16,6 +15,8 @@ import { SnackbarNotification } from '../../../../../shared/services/snackbar-no
 import { AlumnosState } from '../../alumnos-estado';
 import { RouterModule } from '@angular/router';
 import { AppRoutes } from '../../../../../shared/enums/routes';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-form',
@@ -39,17 +40,21 @@ export class EditForm implements OnInit {
 
   searchForm!: FormGroup;
   editForm!: FormGroup;
-  selectedStudent: Student | null = null;
 
-  students: Student[] = [];
+  selectedStudent$ = new BehaviorSubject<Student | null>(null);
+  loading$ = new BehaviorSubject<boolean>(false);
+  students$: Observable<Student[]>;
 
-  loading = false;
+  private studentsValue: Student[] = [];
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    // Inicializar el observable de students y guardar el valor más reciente
+    this.students$ = this.alumnosState.students$.pipe(
+      tap((students) => (this.studentsValue = students))
+    );
+  }
 
   ngOnInit(): void {
-    this.students = this.alumnosState.getStudents();
-
     this.searchForm = this.fb.group({
       dni: ['', [Validators.required, Validators.pattern(/^[1-9]\d{6,7}$/)]],
     });
@@ -79,10 +84,11 @@ export class EditForm implements OnInit {
 
   onSearch() {
     const dni = Number(this.searchForm.value.dni);
-    const student = this.students.find((student) => student.dni === dni);
+    // Usamos studentsValue en lugar de students
+    const student = this.studentsValue.find((student) => student.dni === dni);
 
     if (student) {
-      this.selectedStudent = student;
+      this.selectedStudent$.next(student);
       this.editForm.patchValue({
         name: student.name,
         surname: student.surname,
@@ -91,36 +97,37 @@ export class EditForm implements OnInit {
       });
     } else {
       this.snackbarNotification.error('Estudiante no encontrado');
-      this.selectedStudent = null;
+      this.selectedStudent$.next(null);
     }
   }
 
-onEdit() {
-  if (this.editForm.valid && this.selectedStudent) {
-    this.loading = true;
+  onEdit() {
+    const currentStudent = this.selectedStudent$.value;
+    if (this.editForm.valid && currentStudent) {
+      this.loading$.next(true);
 
-    const editedStudent: Student = {
-      ...this.selectedStudent,
-      ...this.editForm.value,
-    };
+      const editedStudent: Student = {
+        ...currentStudent,
+        ...this.editForm.value,
+      };
 
-    this.alumnosState.editStudent(editedStudent).subscribe({
-      next: () => {
-        this.snackbarNotification.success('Estudiante editado con éxito!');
-        this.onReset();
-        this.loading = false;
-      },
-      error: (err: unknown) => {
-        this.snackbarNotification.error('Error al editar el estudiante');
-        this.loading = false;
-      },
-    });
+      this.alumnosState.editStudent(editedStudent).subscribe({
+        next: () => {
+          this.snackbarNotification.success('Estudiante editado con éxito!');
+          this.onReset();
+          this.loading$.next(false);
+        },
+        error: (err: unknown) => {
+          this.snackbarNotification.error('Error al editar el estudiante');
+          this.loading$.next(false);
+        },
+      });
+    }
   }
-}
 
   onReset() {
     this.searchForm.reset();
     this.editForm.reset();
-    this.selectedStudent = null;
+    this.selectedStudent$.next(null);
   }
 }
